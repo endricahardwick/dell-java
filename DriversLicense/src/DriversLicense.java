bef49fb3b5bd2be424d3643c8a9e04f055760e76
Hardwick <Endrica_Hardwick@Dell.com>
2019-01-28 16:05:51 -0600

    DriversLicense

---
+++ DriversLicense/src/DriversLicense.java
@@ -0,0 +1,70 @@
+
+public class DriversLicense {
+
+// Attributes
+		public String firstName;
+		public String lastName;
+		public int DOB;
+		public int height;
+		public String gender;
+		
+//		Constructor
+		public DriversLicense () {
+			
+		}
+		
+//		Get and Sets
+
+	    public String getFirstName() {
+			return firstName;
+		}
+
+		public void setFirstName(String firstName) {
+			this.firstName = firstName;
+		}
+
+		public String getLastName() {
+			return lastName;
+		}
+
+		public void setLastName(String lastName) {
+			this.lastName = lastName;
+		}
+
+		public int getDOB() {
+			return DOB;
+		}
+
+		public void setDOB(int dOB) {
+			DOB = dOB;
+		}
+
+		public int getHeight() {
+			return height;
+		}
+
+		public void setHeight(int height) {
+			this.height = height;
+		}
+
+		public String getGender() {
+			return gender;
+		}
+
+		public void setGender(String gender) {
+			this.gender = gender;
+		}
+
+		
+//		Methods
+		
+	    public void getFullName() {
+
+	    }
+	    
+		public void getAge() {
+
+	    }
+
+	    
+}
