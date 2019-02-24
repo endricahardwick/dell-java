96006a046ecbae22a24e65ab637e0d509beb1b4b
Hardwick <Endrica_Hardwick@Dell.com>
2019-02-24 16:16:58 -0600

    Fix WordGuess

--- WordGuess/src/WordGuessGame.java
+++ WordGuess/src/WordGuessGame.java
@@ -57,52 +57,53 @@ public class WordGuessGame {
 			filler[iterations] = '-';
 			iterations++;
 		}
-
 		System.out.println(filler);
 
 		System.out.println("Guess a single lowercae character: ");
+
 		while (!wordGuessed) {
 
 // 			Get player's play from input
-			char personGuess = reader.next().charAt(0);
-			
-			
+			String personGuess = reader.next();
+
 //			Check If the player inputs a non-alphabetic character
 //			notify the player that their input is invalid and prompt them to try again.
 			boolean inAlphabet = true;
-//			boolean test = false;
-			inAlphabet = Character.isLetter(personGuess);
-
+			inAlphabet = Character.isLetter(personGuess.charAt(0));
 
 			if (!inAlphabet) {
 				System.out.println("Not a valid entry. Try Again.\n" + "Guess a single lowercase character: ");
 			} else
 
-				
-//			Check if inputs more than one character at a time ???? 
-//			I have no clue on how to do this....several hours spent googling with no luck...
-//			Tried below code with help from class tutor and no luck either...
-				
-//			test = reader.hasNext();
-//			System.out.println(test);
-//				
-//			if (reader.hasNext()) {
-//				System.out.println("Not a valid entry. Try Again.\n" + "Guess a single lowercase character: ");
-//			}
+//				Check if inputs more than one character at a time
+//				notify the player that their input is invalid and prompt them to try again.
+			if (personGuess.length() != 1) {
+				System.out.println("Not a valid entry. Try Again.\n" + "Guess a single lowercase character: ");
+			} else
 
-				
+//			Check if character has already been guessed.
+//			notify the player that their input is a duplicate and prompt them to try again.
+			if (String.valueOf(filler).contains(personGuess + "")) {
+				System.out.println(
+						"Character has already been guessed. Try Again.\n" + "Guess a single lowercase character: ");
+			} else
+
 //			Check if character in chosen word and if found replace "-" with character
 			if (chosenWord.contains(personGuess + "")) {
 				for (int i = 0; i < chosenWordLength; i++) {
-					if (chosenWord.charAt(i) == personGuess) {
-						filler[i] = personGuess;
+					if (chosenWord.charAt(i) == personGuess.charAt(0)) {
+						filler[i] = personGuess.charAt(0);
 					}
 				}
+//				notify the player that their input is valid and prompt them for another guess.
+				System.out.println("Awesome!\n" + "Guess another single lowercase character: ");
 			} else {
+//				notify the player that their input is invalid and prompt them to try again.
 				System.out.println("No Match Found. Try Again.\n" + "Guess a single lowercase character: ");
 			}
 
 //			Check if filler equals chosenWord
+//			If word is guessed, notify the player by congradulating them. Game Over.
 			if (chosenWord.equals(String.valueOf(filler))) {
 				System.out.println();
 				System.out.println("Congrats! You guessed the word. Game Over!");
